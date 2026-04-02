73aa8d80a008d3380123a2aba1230484849254bb
bananpedor1488 <bananpedor1488@gmail.com>
2026-04-02 16:14:40 +0300

    Update ChatView.swift

--- Jemmy/Jemmy/Views/ChatView.swift
+++ Jemmy/Jemmy/Views/ChatView.swift
@@ -176,7 +176,7 @@ struct MessageBubble: View {
                     .background(isFromMe ? Color.green.opacity(0.8) : Color.white.opacity(0.1))
                     .cornerRadius(16)
                 
-                Text(formatTime(message.createdAt))
+                Text(formatTime(message.createdDate))
                     .font(.system(size: 12))
                     .foregroundColor(.white.opacity(0.5))
                     .padding(.horizontal, 4)
