2a088968fece03f00bff1bd4035e2b7007c14be0
bananpedor1488 <bananpedor1488@gmail.com>
2026-04-02 16:24:41 +0300

    Update ChatsListView.swift

--- Jemmy/Jemmy/Views/ChatsListView.swift
+++ Jemmy/Jemmy/Views/ChatsListView.swift
@@ -108,6 +108,7 @@ struct ChatsListView: View {
             }
             .navigationTitle("Чаты")
             .navigationBarTitleDisplayMode(.large)
+            .toolbar(.visible, for: .tabBar)
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Button(action: { showSearchByTag = true }) {
